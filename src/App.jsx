84ca5fdc2bf52cc8fa79389aa1b4128390cfb04e
AnotheerDev <leger.julien94@gmail.com>
2024-02-09 10:35:44 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+import Header from './components/Header/Header'
+
+function App() {
+
+  return (
+    <main>
+      <Header />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <span className=' block text-center'>
+        livraison offerte à partir de 100 euros d'achat
+      </span>
+    </header>
+  );
+}
+
+export default Header;
