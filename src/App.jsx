b00ed99d4a4ac45f93dc92f0d893eec42bb15718
AnotheerDev <leger.julien94@gmail.com>
2024-02-09 18:39:45 +0100

    Add Product component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './index.css'
 import Header from './components/Header/Header'
 import Intro from './components/Intro/Intro'
+import Product from './components/Product/Product'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <main>
       <Header />
       <Intro />
+      <Product />
     </main>
   )
 }
