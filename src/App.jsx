fd6904c593df32d9ae497129b7068a2bfe7d24a6
AnotheerDev <leger.julien94@gmail.com>
2024-02-09 15:49:15 +0100

    Add Intro component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './index.css'
 import Header from './components/Header/Header'
+import Intro from './components/Intro/Intro'
 
 function App() {
 
   return (
     <main>
       <Header />
+      <Intro />
     </main>
   )
 }
