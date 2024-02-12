c1ec3443651e7ee3a7988196c944ef116923ab9d
AnotheerDev <leger.julien94@gmail.com>
2024-02-12 18:23:20 +0100

    Add Marques component

---
+++ src/components/Marques/Marques.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Marques = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold text-left mx-60">
+        <span className="border-b-2 border-red-500">NO</span>S MARQUES PHARES
+      </h2>
+    </div>
+  );
+};
+
+export default Marques;
