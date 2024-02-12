3478a65efcf79d1a17e915a18536b00431f74690
AnotheerDev <leger.julien94@gmail.com>
2024-02-12 20:30:33 +0100

    Add Zoom component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header/Header'
 import Intro from './components/Intro/Intro'
 import Product from './components/Product/Product'
 import Marques from './components/Marques/Marques'
+import Zoom from './components/Zoom/Zoom'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Intro />
       <Product />
       <Marques />
+      <Zoom />
     </main>
   )
 }
