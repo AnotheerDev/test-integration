193f414b6b0a809679797b90c1ceacc7f991c711
AnotheerDev <leger.julien94@gmail.com>
2024-02-15 12:25:00 +0100

    Refactor Connecter component to include email input and icon

--- src/components/Connecter/Connecter.jsx
+++ src/components/Connecter/Connecter.jsx
@@ -1,37 +1,24 @@
 import React from "react";
+import { FaRegEnvelope } from "react-icons/fa";
 
 const Revendeur = () => {
   return (
-    <div className="bg-red-700 text-white py-8 px-64 flex items-center"> 
+    <div className="bg-red-700 text-white py-8 px-64 flex items-center">
       <div className="flex-1 pr-8">
-        <h3 className="text-3xl font-bold">
-          Restons connectés
-        </h3>
+        <h3 className="text-3xl font-bold">Restons connectés</h3>
         <p className="my-4 font-thin">
-          Ne ratez aucune informations sur les produits UTF en vous inscrivant à notre newsletter !
+          Ne ratez aucune informations sur les produits UTF en vous inscrivant à
+          notre newsletter !
         </p>
       </div>
       <div className="flex-1 pl-8">
-        <div className="relative">
+        <div className="relative text-white">
           <input
             type="text"
             placeholder="Renseigner votre adresse email"
-            className="w-full bg-red-700 px-4 py-2 border-b border-white"
+            className="w-full bg-red-700 pl-4 pr-10 py-2 border-b border-white text-white"
           />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M19 19l-6-6M7 11a5 5 0 1 0 0-2 5 5 0 0 0 0 2z"
-            />
-          </svg>
+          <FaRegEnvelope className="absolute right-3 top-1/2 transform -translate-y-1/2 text-xl" />
         </div>
       </div>
     </div>
