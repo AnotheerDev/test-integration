3df065be1e435ab254ec7344b2cbc6837e3a2e66
AnotheerDev <leger.julien94@gmail.com>
2024-02-18 00:00:52 +0100

    Update Zoom component styles for small device

--- src/components/Zoom/Zoom.jsx
+++ src/components/Zoom/Zoom.jsx
@@ -4,12 +4,12 @@ import bgZoom from "/img/bgzoom.jpg";
 const Zoom = () => {
   return (
     <section className="flex flex-col my-32">
-      <h2 className="text-2xl font-bold mb-12 mx-60">
+      <h2 className="text-2xl font-bold mb-12 mx-4 lg:mx-60">
         <span className="border-b-4 border-red-500">ZO</span>OM SUR
       </h2>
 
       <div
-        className="py-52 px-4 relative"
+        className="py-32 lg:py-52 lg:px-4 relative"
         style={{
           backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('${bgZoom}')`,
           backgroundRepeat: "no-repeat",
@@ -20,7 +20,7 @@ const Zoom = () => {
         <div className=" text-white text-center">
           <p className="font-thin text-xs">MEGABASS</p>
           <p className="font-bold text-2xl pb-4 pt-2">LE LIVE X REVENGE, UN VERITABLE BIJOU</p>
-          <p className="font-thin w-2/6 m-auto ">
+          <p className="font-thin lg:w-2/6 m-auto ">
             Des vibrations importants, une sonorité tungstène, une nage
             parfaite, et une capacité à réagir immédiatement au maniement...
             c'est Live X Revenge
