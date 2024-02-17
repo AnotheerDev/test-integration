d0442aec0982482cbb8829192a4d5845e202fd52
AnotheerDev <leger.julien94@gmail.com>
2024-02-17 22:52:39 +0100

    Update Connecter component styling for small device

--- src/components/Connecter/Connecter.jsx
+++ src/components/Connecter/Connecter.jsx
@@ -3,22 +3,22 @@ import { FaRegEnvelope } from "react-icons/fa";
 
 const Revendeur = () => {
   return (
-    <section className="bg-red-700 text-white py-8 px-64 flex items-center">
-      <div className="flex-1 pr-8">
+    <section className="bg-red-700 text-white py-10 px-4 lg:py-8 lg:px-64 lg:flex lg:items-center">
+      <div className="lg:flex-1 lg:pr-8">
         <h3 className="text-3xl font-bold">Restons connectés</h3>
-        <p className="my-4 font-thin">
+        <p className="py-4 lg:py-0 lg:my-4 font-thin">
           Ne ratez aucune informations sur les produits UTF en vous inscrivant à
           notre newsletter !
         </p>
       </div>
-      <div className="flex-1 pl-8">
-        <div className="relative text-white">
+      <div className="flex-1">
+        <div className="flex items-center bg-red-700 pl-4  py-2 border-b border-white text-white">
           <input
             type="text"
             placeholder="Renseigner votre adresse email"
-            className="w-full bg-red-700 pl-4 pr-10 py-2 border-b border-white text-white"
+            className="w-auto bg-transparent flex-1 "
           />
-          <FaRegEnvelope className="absolute right-3 top-1/2 transform -translate-y-1/2 text-xl" />
+          <FaRegEnvelope className="text-xl" />
         </div>
       </div>
     </section>
