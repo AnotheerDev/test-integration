1f3affb074055cc2144479179473e4136f4fca3a
AnotheerDev <leger.julien94@gmail.com>
2024-02-17 22:07:43 +0100

    Update Footer component styling for small device

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,24 +10,20 @@ import logo from "/img/logo.png";
 
 const Footer = () => {
   return (
-    <footer className="bg-slate-900 text-white pt-28 pb-8 px-32">
-      <div className="flex justify-start border-b border-gray-700 pb-8">
+    <footer className="bg-slate-900 text-white px-6 lg:pt-28 lg:pb-8 lg:px-32">
+      <div className="lg:flex lg:justify-start lg:pb-8">
         <div>
-          <div>
-            <img
-              src={logo}
-              alt="Ultimate Fishing Logo"
-              className="w-60"
-            />
+          <div className="flex justify-center">
+            <img src={logo} alt="Ultimate Fishing Logo" className="w-60 " />
           </div>
         </div>
-        <div className="space-y-2 ml-10 mr-32">
+        <div className="py-2 space-y-2 lg:ml-10 lg:mr-32">
           <h5 className="font-bold">ULTIMATE FISHING</h5>
           <p>Qui sommes-nous</p>
           <p>Le blog</p>
           <p>Trouver un revendeur</p>
         </div>
-        <div className="space-y-2">
+        <div className="py-4 space-y-2">
           <h5 className="font-bold">NOUS CONTACTER</h5>
           <p>Nous écrire</p>
           <p>02.97.31.27.89</p>
@@ -35,9 +31,9 @@ const Footer = () => {
           <p>56360 Le Palais</p>
         </div>
       </div>
-      <div className="pt-8 flex items-center">
-        <p className="mr-4">Nos réseaux sociaux</p>
-        <div className="flex items-center gap-x-14">
+      <div className="py-2 border-b-2 border-t-2 border-white lg:py-6 flex lg:items-center">
+        <p className="hidden lg:block lg:mr-4">Nos réseaux sociaux</p>
+        <div className="flex items-center gap-x-10 lg:gap-x-14">
           <FaFacebookF className="text-2xl" />
           <FaInstagram className="text-2xl" />
           <FaYoutube className="text-2xl" />
@@ -46,10 +42,12 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="flex justify-center border-t border-gray-700 pt-4 mt-8">
-        <div className="flex space-x-8">
+      <div className="flex flex-col lg:flex-row justify-center py-8 lg:pt-4 lg:mt-8 gap-4">
+        <div className="flex justify-center gap-4">
           <p>CGV</p>
           <p>Mentions légales</p>
+        </div>
+        <div className="flex justify-center gap-4">
           <p>Ultimate Fishing</p>
           <p>2024 - Made by AT14</p>
         </div>
