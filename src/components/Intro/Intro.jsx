97829331c9e74d83ef50a00d83257e2709af4ac4
AnotheerDev <leger.julien94@gmail.com>
2024-02-18 13:17:03 +0100

    Refactor Intro component styling for small device

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -2,23 +2,18 @@ import React from "react";
 
 const Intro = () => {
   return (
-    <div className=" text-white py-8 relative" style={{ height: "50vh"}}>
-      <div className="absolute inset-0 bg-[url('/public/img/ultimatefishing.jpg')] bg-cover bg-center " style={{backgroundSize: "125%", backgroundRepeat: "no-repeat" }}></div>
-      <div className="absolute inset-0 flex justify-start items-center">
-        {/* Texte */}
-        <div className="w-3/5 relative z-10">
-          <div className="text-left px-40">
-            <h1 className="text-3xl font-bold">ULTIMATE FISHING</h1>
-            <p className="my-6 w-80">
-              Le meilleur matériel de pêche pour les passionnés de pêche aux leurres
-            </p>
-            <button className='text-xl py-3 px-7 bg-white text-black'>Nos produits</button>
-          </div>
-        </div>
-      </div>
-      {/* Gradiant avec opacité sur l'image de fond */}
-      <div className="absolute inset-0" style={{ background: "linear-gradient(to right, rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 0) 100%)", width: "70%" }}></div>
+<div className="text-white py-8 relative" style={{ height: "50vh" }}>
+  <div className="absolute inset-0 bg-[url('/public/img/ultimatefishing.jpg')] bg-cover bg-center"></div>
+  <div className="absolute inset-0 flex justify-start items-center">
+    <div className="w-3/5 relative z-10 px-10 lg:px-40">
+      <h1 className="text-3xl font-bold">ULTIMATE FISHING</h1>
+      <p className="my-6 w-80">Le meilleur matériel de pêche pour les passionnés de pêche aux leurres</p>
+      <button className='text-xl font-semibold py-3 px-7 bg-white text-black lg:font-normal'>Nos produits</button>
     </div>
+  </div>
+  <div className="absolute inset-0 bg-gradient-to-r from-black to-transparent"></div>
+</div>
+
   );
 }
 
